a6cc14fc7c3093ff75f2556f88991b6fc01049c4
Sampoornnagpal <nagpalsampoorn@gmail.com>
2025-07-18 10:55:27 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,22 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const BugBounty = await hre.ethers.getContractFactory("BugBounty");
+  const bugBounty = await BugBounty.deploy();
+  await bugBounty.waitForDeployment();
+
+  const address = await bugBounty.getAddress();
+  console.log("Deployed to:", address);
+
+  fs.writeFileSync(
+    "./frontend/contracts/contract-address.json",
+    JSON.stringify({ BugBounty: address }, null, 2)
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
