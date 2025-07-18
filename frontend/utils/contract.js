ec45492bce6fb7d34c2b25f43a4398b0a512a8a8
Sampoornnagpal <nagpalsampoorn@gmail.com>
2025-07-18 10:55:50 +0530

    Update contract.js

--- frontend/utils/contract.js
+++ frontend/utils/contract.js
@@ -1,1 +1,10 @@
+import { ethers } from "ethers";
+import BugBountyABI from "../contracts/BugBounty.json";
+import contractAddress from "../contracts/contract-address.json";
+
+export const getBugBountyContract = async () => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  return new ethers.Contract(contractAddress.BugBounty, BugBountyABI.abi, signer);
+};
 
